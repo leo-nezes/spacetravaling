32db13f062f99edb588615d56edadb8c96ef7d6d
Leonardo Menezes <leonhard.menezes@gmail.com>
2021-06-18 10:03:22 -0300

    feat(Header): Passing in tests - should be able to navigate to home page after a click

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="logo" />
-        <h2>spacetraveling</h2>
-        <span>.</span>
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="logo" />
+            <h2>spacetraveling</h2>
+            <span>.</span>
+          </a>
+        </Link>
       </div>
     </header>
   );
